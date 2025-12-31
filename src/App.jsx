4c2352b65eae66dafaa8f6070d70140a882d6f07
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2025-12-31 17:37:52 +0530

    First Commited By Niv Vasani

---
+++ src/App.jsx
@@ -0,0 +1,235 @@
+//? this is compulsory for used zTo ANTDesign
+
+import "antd/dist/antd.css";
+import "./App.css";
+import { useState } from "react";
+import {
+  // EditOutlined For Edit User Data
+  EditOutlined,
+  DeleteOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+import { Button, Table, Modal, Input } from "antd";
+
+function App() {
+  //* Fetch the Data or Data source
+  const [dataSource, setDataSource] = useState([
+    {
+      id: 1,
+      name: "Nirav",
+      email: "niravvasani12@gmail.com",
+      address: "Surat Gujarat",
+      date: "2025-01-10",
+    },
+    {
+      id: 2,
+      name: "Kamal bhai",
+      email: "kamal123@gmail.com",
+      address: "jaipur Rajasthan",
+      date: "2025-01-15",
+    },
+    {
+      id: 3,
+      name: "Nikunj Bhai",
+      email: "Nikunj123@gmail.com",
+      address: "Ahemdabad Gujarat",
+      date: "2025-01-12",
+    },
+    {
+      id: 4,
+      name: "Kiran",
+      email: "kiran123@gmail.com",
+      address: "Amreli Gujarat",
+      date: "2025-01-08",
+    },
+  ]);
+
+  const columns = [
+    //* key show How many column are addeed in our table
+    // ? 1. filterDropdown is used to Search the data of record
+    // ? 2. DeletedOutlined is used to delete the data from record
+    // ? 3. EditedOutlined is used to Edit the data from record
+    // ? 4. const today = new Date().toISOString().split("T")[0]; is used TO sort data by its date
+
+    {
+      key: "1",
+      title: "ID",
+      dataIndex: "id",
+    },
+    {
+      key: "2",
+      title: "Name",
+      dataIndex: "name",
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => {
+        return (
+          <>
+            <Input
+              autoFocus
+              placeholder="Type to Search "
+              value={selectedKeys[0]}
+              onChange={(e) => {
+                setSelectedKeys(e.target.value ? [e.target.value] : []);
+                confirm({ closeDropdown: false });
+              }}
+              onPressEnter={() => confirm()}
+              onBlur={() => confirm()}
+            />
+            <Button onClick={() => confirm()} type="primary">
+              Search
+            </Button>
+            <Button onClick={() => clearFilters()} type="danger">
+              Reset
+            </Button>
+          </>
+        );
+      },
+      filterIcon: () => <SearchOutlined />,
+      onFilter: (value, record) =>
+        record.name.toLowerCase().includes(value.toLowerCase()),
+    },
+    {
+      key: "3",
+      title: "Email",
+      dataIndex: "email",
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => {
+        return (
+          <>
+            <Input
+              autoFocus
+              placeholder="Type to Search "
+              value={selectedKeys[0]}
+              onChange={(e) => {
+                setSelectedKeys(e.target.value ? [e.target.value] : []);
+                confirm({ closeDropdown: false });
+              }}
+              onPressEnter={() => confirm()}
+              onBlur={() => confirm()}
+            />
+            <Button onClick={() => confirm()} type="primary">
+              Search
+            </Button>
+            <Button onClick={() => clearFilters()} type="danger">
+              Reset
+            </Button>
+          </>
+        );
+      },
+      filterIcon: () => <SearchOutlined />,
+      onFilter: (value, record) =>
+        record.email.toLowerCase().includes(value.toLowerCase()),
+    },
+    {
+      key: "4",
+      title: "Address",
+      dataIndex: "address",
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => {
+        return (
+          <>
+            <Input
+              autoFocus
+              placeholder="Type to Search "
+              value={selectedKeys[0]}
+              onChange={(e) => {
+                setSelectedKeys(e.target.value ? [e.target.value] : []);
+                confirm({ closeDropdown: false });
+              }}
+              onPressEnter={() => confirm()}
+              onBlur={() => confirm()}
+            />
+            <Button onClick={() => confirm()} type="primary">
+              Search
+            </Button>
+            <Button onClick={() => clearFilters()} type="danger">
+              Reset
+            </Button>
+          </>
+        );
+      },
+      filterIcon: () => <SearchOutlined />,
+      onFilter: (value, record) =>
+        record.address.toLowerCase().includes(value.toLowerCase()),
+    },
+
+    {
+      key: "5",
+      title: "Date By sorted",
+      dataIndex: "date",
+      sorter: (a, b) => new Date(a.date) - new Date(b.date),
+      sortDirections: ["ascend", "descend"],
+    },
+
+    {
+      key: "6",
+      title: "Delete",
+      render: (record) => {
+        return (
+          <DeleteOutlined
+            onClick={() => onDeleteUser(record)}
+            style={{ color: "red" }}
+          />
+        );
+      },
+    },
+  ];
+
+  const onAddUser = () => {
+    const randNumber = Math.floor(Math.random() * 100);
+
+    const randomLetter = Array.from({ length: 5 }, () =>
+      String.fromCharCode(65 + Math.floor(Math.random() * 26))
+    ).join("");
+
+    const today = new Date().toISOString().split("T")[0];
+
+    const newUser = {
+      id: randNumber,
+      name: randomLetter,
+      email: randomLetter + randNumber + "@gmail.com",
+      address: randomLetter + randNumber,
+      date: today,
+    };
+
+    setDataSource((pre) => {
+      return [...pre, newUser];
+    });
+  };
+
+  const onDeleteUser = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this user/User data",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((user) => user.id !== record.id);
+        });
+      },
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button onClick={onAddUser}>Add New User From Here</Button>
+        <Table columns={columns} dataSource={dataSource} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
