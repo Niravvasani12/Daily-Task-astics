d3c0753fcc4b648c8aa3a15619d6344373ad7e02
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2026-01-20 15:58:57 +0530

    ATM demo with PDF receipt

--- src/App.jsx
+++ src/App.jsx
@@ -13,36 +13,7 @@ import { Button, Table, Modal, Input } from "antd";
 
 function App() {
   //* Fetch the Data or Data source
-  const [dataSource, setDataSource] = useState([
-    {
-      id: 1,
-      name: "Nirav",
-      email: "niravvasani12@gmail.com",
-      address: "Surat Gujarat",
-      date: "2025-01-10",
-    },
-    {
-      id: 2,
-      name: "Kamal bhai",
-      email: "kamal123@gmail.com",
-      address: "jaipur Rajasthan",
-      date: "2025-01-15",
-    },
-    {
-      id: 3,
-      name: "Nikunj Bhai",
-      email: "Nikunj123@gmail.com",
-      address: "Ahemdabad Gujarat",
-      date: "2025-01-12",
-    },
-    {
-      id: 4,
-      name: "Kiran",
-      email: "kiran123@gmail.com",
-      address: "Amreli Gujarat",
-      date: "2025-01-08",
-    },
-  ]);
+  const [dataSource, setDataSource] = useState([]);
 
   const columns = [
     //* key show How many column are addeed in our table
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,1 +1,58 @@
 //* Create Mobx for State Management.
+
+import { makeAutoObservable } from "mobx";
+
+class UserStore {
+  users = [];
+  loading = false;
+
+  // constructor is automatically created when class is created.anticon.anticon-star
+
+  constructor() {
+    makeAutoObservable(this);
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.loading = true;
+    setTimeout(() => {
+      this.users = [
+        {
+          id: 1,
+          name: "Nirav",
+          email: "niravvasani12@gmail.com",
+          address: "Surat Gujarat",
+          date: "2025-01-10",
+        },
+        {
+          id: 2,
+          name: "Kamal bhai",
+          email: "kamal123@gmail.com",
+          address: "jaipur Rajasthan",
+          date: "2025-01-15",
+        },
+        {
+          id: 3,
+          name: "Nikunj Bhai",
+          email: "Nikunj123@gmail.com",
+          address: "Ahemdabad Gujarat",
+          date: "2025-01-12",
+        },
+        {
+          id: 4,
+          name: "Kiran",
+          email: "kiran123@gmail.com",
+          address: "Amreli Gujarat",
+          date: "2025-01-08",
+        },
+      ];
+      this.loading = false;
+    }, 2000);
+  }
+
+  addUser() {}
+
+  deleteUser() {}
+}
+
+export default new UserStore();
